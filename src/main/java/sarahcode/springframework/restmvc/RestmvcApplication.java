a8fadff501aedb5f030a216680b02b0515043739
Davi Silva <davisilvaphoto@gmail.com>
2025-09-01 08:57:06 -0300

    lint file

--- src/main/java/sarahcode/springframework/restmvc/RestmvcApplication.java
+++ src/main/java/sarahcode/springframework/restmvc/RestmvcApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestmvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestmvcApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(RestmvcApplication.class, args);
+  }
 }
