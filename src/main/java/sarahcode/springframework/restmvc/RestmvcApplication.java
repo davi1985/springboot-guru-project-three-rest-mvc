647152337422acabf0a6b48ce55c8f93d7db54ee
Davi Silva <davisilvaphoto@gmail.com>
2025-09-01 08:55:30 -0300

    init files

---
+++ src/main/java/sarahcode/springframework/restmvc/RestmvcApplication.java
@@ -0,0 +1,13 @@
+package sarahcode.springframework.restmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sarahcode/springframework/restmvc/RestmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package sarahcode.springframework.restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
