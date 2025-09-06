749170d27c5e7770a4d7fa7ea8729b28a962f762
Davi Silva <davisilvaphoto@gmail.com>
2025-09-06 08:17:21 -0300

    get by id using path variable

--- src/main/java/sarahcode/springframework/restmvc/controller/BeerController.java
+++ src/main/java/sarahcode/springframework/restmvc/controller/BeerController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import sarahcode.springframework.restmvc.model.Beer;
 import sarahcode.springframework.restmvc.services.BeerService;
@@ -12,16 +15,18 @@ import sarahcode.springframework.restmvc.services.BeerService;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
   private final BeerService beerService;
 
-  @RequestMapping("/api/v1/beer")
+  @RequestMapping(method = RequestMethod.GET)
   public List<Beer> beerList() {
     return beerService.beerList();
   }
 
-  public Beer getBeerById(UUID id) {
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public Beer getBeerById(@PathVariable("id") UUID id) {
     log.debug("Get Beer Id in controller");
 
     return beerService.getBeerById(id);
