76e2e37d351c222463216894923c803c2f85ffbf
Davi Silva <davisilvaphoto@gmail.com>
2025-09-20 10:25:21 -0300

    http put

--- src/main/java/sarahcode/springframework/restmvc/controller/BeerController.java
+++ src/main/java/sarahcode/springframework/restmvc/controller/BeerController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,8 +46,13 @@ public class BeerController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Beer getBeerById(@PathVariable("id") UUID id) {
-        log.debug("Get Beer Id in controller");
-
         return beerService.getBeerById(id);
+    }
+
+    @PutMapping("{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerUuid, @RequestBody Beer beer) {
+        beerService.updateById(beerUuid, beer);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
